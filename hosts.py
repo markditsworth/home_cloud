f35f8c5d3d1cae4c326b61b394e69bbc20e6e608
Mark Ditsworth <markditsworth@protonmail.com>
2022-02-18 00:17:55 -0600

    update hosts with emby server

--- hosts.py
+++ hosts.py
@@ -9,7 +9,8 @@ BROADCAST = "192.168.1.0/24"
 
 MAC_ADDRESSES = {
     "e4:5f:01:73:87:6a": "controller",
-    "dc:a6:32:d6:cf:fb": "monitor"
+    "dc:a6:32:d6:cf:fb": "monitor",
+    "dc:a6:32:bb:cb:50": "emby",
 }
 
 def mapDevices(broadcast_addr=BROADCAST):
