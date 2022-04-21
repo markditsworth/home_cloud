c3d4ebffb738ae86fea962a2fc71579b7220b2a7
Mark Ditsworth <markditsworth@protonmail.com>
2022-04-20 22:36:21 -0500

    update docker job

--- roles/openvpn/files/validate.sh
+++ roles/openvpn/files/validate.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-tr_result=$(traceroute google.com | grep '^ 1 ' | awk '{ print $2 }' | awk -F. '{ print $1 }')
-
-if [ "${tr_result}" == '10' ]; then
-    exit 0
-else
-    exit 1
-fi
+curl -s ipinfo.io/$(curl -s ifconfig.me) | jq -r '.country'
